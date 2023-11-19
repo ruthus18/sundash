5976d3b44a4f12293ab8bd4764c6db6bf357f874
Тряпчев Данил Павлович <tryapchev@tochka.com>
2023-11-19 21:24:47 +0500

    update tmp fix with multiplayer, add more buttons

--- examples/_01_counter.py
+++ examples/_01_counter.py
@@ -10,24 +10,31 @@ app = App()
 
 
 class Counter(Component):
-    html = '<b>{{ count }}</b> <button id="{{ conn_id }}">+</button>'
+    btn_minus = '<button id="minus">-</button>'
+    btn_plus = '<button id="plus">+</button>'
+    html = '   '.join((btn_minus, '<b>{{ count }}</b>', btn_plus))
 
     @dataclass
     class Vars:
-        conn_id: int
         count: int = 0
 
     def __init__(self):
-        conn_id = get_connection().id
-        self.vars: dict = self.Vars(conn_id).__dict__
+        super().__init__()
+        self.conn_id = get_connection().id
 
     @on(BUTTON_CLICK)
-    async def update(self, _):
+    async def update(self, sig: BUTTON_CLICK):
         conn_id = get_connection().id
-        if conn_id != self.vars['conn_id']:
+        if conn_id != self.conn_id:
             return
 
-        await self.set('count', self.vars['count'] + 1)
+        if 'plus' == sig.button_id:
+            self.vars['count'] += 1
+
+        elif 'minus' == sig.button_id:
+            self.vars['count'] -= 1
+
+        await self.set('count', self.vars['count'])
 
 
 app.run(layout=('<h1>🧮 Counter</h1>', Counter))
